685493b77dc2570b5a40156f571b2c6a7f2b0eec
Aaron <aaron.tan1@gmail.com>
2025-02-26 16:20:57 +0800

    feat: Implement dynamic file routes and display uploaded files with file cards

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "standard", "plugin:tailwindcss/recommended", "prettier"),
+  ...compat.rules({
+    "no-undef": "off",
+  }),
 ];
 
 export default eslintConfig;
